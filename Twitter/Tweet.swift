5c0a30a8d764ee2120147a25396304e6496a15e1
Alan Hamlett <alan.hamlett@airbnb.com>
2017-04-13 15:49:21 -0700

    hookup data to tweet table cell

--- Twitter/Tweet.swift
+++ Twitter/Tweet.swift
@@ -10,40 +10,47 @@ import UIKit
 
 class Tweet: NSObject {
 
-  let tweetId: Int
-  let text: String?
-  let timestamp: NSDate?
-  let retweetCount: Int
-  let favoritesCount: Int
-  let userName: String?
-  let userScreenName: String?
-  let userImageURL: URL?
+  var tweetId: Int?
+  var text: String?
+  var createdAt: Date?
+  var retweetCount: Int?
+  var favoritesCount: Int?
+  var userName: String?
+  var userScreenName: String?
+  var userImageURL: URL?
+  var isRetweet: Bool
+  var retweetingUserName: String?
 
   init(dictionary:NSDictionary) {
 
-    tweetId = dictionary["id"] as! Int
+    tweetId = dictionary["id"] as? Int
     text = dictionary["text"] as? String
     if let date = dictionary["created_at"] as? String {
       let formatter = DateFormatter()
       formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
-      timestamp = formatter.date(from: date) as NSDate?
-    } else {
-      timestamp = nil
+      createdAt = formatter.date(from: date)!
     }
     retweetCount = (dictionary["retweet_count"] as? Int) ?? 0
     favoritesCount = (dictionary["favourites_count"] as? Int) ?? 0
-    if let user = dictionary["user"] as? NSDictionary {
-      userName = user["name"] as? String
-      userScreenName = user["scren_name"] as? String
-      if let url = user["profile_image_url_https"] as? String {
-        userImageURL = URL(string: url)
-      } else {
-        userImageURL = nil
+    if let retweet = dictionary["retweeted_status"] as? NSDictionary {
+      isRetweet = true
+      if let user = retweet["user"] as? NSDictionary {
+        retweetingUserName = user["name"] as? String
+        userName = user["name"] as? String
+        userScreenName = user["screen_name"] as? String
+        if let url = user["profile_image_url_https"] as? String {
+          userImageURL = URL(string: url)!
+        }
       }
     } else {
-      userName = ""
-      userScreenName = ""
-      userImageURL = nil
+      isRetweet = false
+      if let user = dictionary["user"] as? NSDictionary {
+        userName = user["name"] as? String
+        userScreenName = user["screen_name"] as? String
+        if let url = user["profile_image_url_https"] as? String {
+          userImageURL = URL(string: url)!
+        }
+      }
     }
   }
 
--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -13,9 +13,34 @@ class TweetCell: UITableViewCell {
   var tweet: Tweet! {
     didSet {
       nameLabel.text = tweet.userName
-      screenNameLabel.text = "@\(tweet.userScreenName)"
+      screenNameLabel.text = "@\(tweet.userScreenName!)"
       tweetContentLabel.text = tweet.text
       avatarImageView.setImageWith(tweet.userImageURL!)
+      if var seconds = tweet.createdAt?.timeIntervalSinceNow {
+        seconds = abs(seconds)
+        if seconds > 60 {
+          seconds /= 60
+          if seconds > 60 {
+            seconds /= 60
+            let duration = String(format: "%.0f", round(seconds))
+            createdAtLabel.text = "\(duration)h"
+          } else {
+            let duration = String(format: "%.0f", round(seconds))
+            createdAtLabel.text = "\(duration)m"
+          }
+        } else {
+          let duration = String(format: "%.0f", round(seconds))
+          createdAtLabel.text = "\(duration)s"
+        }
+      }
+      if tweet.isRetweet {
+        retweetedByNameLabel.text = "\(tweet.retweetingUserName!) retweeted"
+        retweetedByImageView.isHidden = false
+        retweetedByNameLabel.isHidden = false
+      } else {
+        retweetedByImageView.isHidden = true
+        retweetedByNameLabel.isHidden = true
+      }
     }
   }
 
@@ -24,6 +49,8 @@ class TweetCell: UITableViewCell {
   @IBOutlet weak var screenNameLabel: UILabel!
   @IBOutlet weak var createdAtLabel: UILabel!
   @IBOutlet weak var tweetContentLabel: UILabel!
+  @IBOutlet weak var retweetedByImageView: UIImageView!
+  @IBOutlet weak var retweetedByNameLabel: UILabel!
 
   override func awakeFromNib() {
     super.awakeFromNib()
